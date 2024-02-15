e9e676a2f94e7422b21e6d69ae0b514b37dd2814
akarshghildyal <129004417+akarshghildyal@users.noreply.github.com>
2024-02-15 22:58:51 +0530

    Update content.js
    added the background color #E6E6FA to the new cells.

--- content.js
+++ content.js
@@ -67,6 +67,7 @@ function everything() {
       if (margin < 0) {
         cell.style.color = "red";
       }
+      cell.style.backgroundColor = "#E6E6FA";
       row.appendChild(cell);
     });
   }
